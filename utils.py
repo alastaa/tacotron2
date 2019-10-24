9226580f9dc78b4a03d5ec2828ff2a4e3b5c27c1
Antti Alastalo <antti.alastalo@aalto.fi>
2019-10-24 14:43:44 +0300

    Added handling of 24-bit wav files.

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import wave
 from scipy.io.wavfile import read
 import torch
 
@@ -14,7 +15,7 @@ def get_mask_from_lengths(lengths, use_cpu):
 
 
 def load_wav_to_torch(full_path):
-    sampling_rate, data = read(full_path)
+    sampling_rate,_, data = readwav(full_path)
     return torch.FloatTensor(data.astype(np.float32)), sampling_rate
 
 
@@ -30,3 +31,44 @@ def to_gpu(x):
     if torch.cuda.is_available():
         x = x.cuda(non_blocking=True)
     return torch.autograd.Variable(x)
+
+
+def _wav2array(nchannels, sampwidth, data):
+    """data must be the string containing the bytes from the wav file."""
+    num_samples, remainder = divmod(len(data), sampwidth * nchannels)
+    if remainder > 0:
+        raise ValueError('The length of data is not a multiple of '
+                         'sampwidth * num_channels.')
+    if sampwidth > 4:
+        raise ValueError("sampwidth must not be greater than 4.")
+
+    if sampwidth == 3:
+        a = np.empty((num_samples, nchannels, 4), dtype=np.uint8)
+        raw_bytes = np.fromstring(data, dtype=np.uint8)
+        a[:, :, :sampwidth] = raw_bytes.reshape(-1, nchannels, sampwidth)
+        a[:, :, sampwidth:] = (a[:, :, sampwidth - 1:sampwidth] >> 7) * 255
+        result = a.view('<i4').reshape(a.shape[:-1])
+    else:
+        # 8 bit samples are stored as unsigned ints; others as signed ints.
+        dt_char = 'u' if sampwidth == 1 else 'i'
+        a = np.fromstring(data, dtype='<%s%d' % (dt_char, sampwidth))
+        result = a.reshape(-1, nchannels)
+    return result
+
+
+def readwav(file):
+    """
+    Read a wav file.
+    Returns the frame rate, sample width (in bytes) and a numpy array
+    containing the data.
+    This function does not read compressed wav files.
+    """
+    wav = wave.open(file)
+    rate = wav.getframerate()
+    nchannels = wav.getnchannels()
+    sampwidth = wav.getsampwidth()
+    nframes = wav.getnframes()
+    data = wav.readframes(nframes)
+    wav.close()
+    array = _wav2array(nchannels, sampwidth, data)
+    return rate, sampwidth, array
