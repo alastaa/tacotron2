ee25ca3651ce6b967ae53b931f029bdf1436da2a
Antti Alastalo <antti.alastalo@aalto.fi>
2019-10-25 17:11:21 +0300

    modified text cleaners and symbol lists

--- text/symbols.py
+++ text/symbols.py
@@ -12,7 +12,7 @@ _special = '-'
 _letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖabcdefghijklmnopqrstuvwxyzåäö'
 
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
-_arpabet = ['@' + s for s in cmudict.valid_symbols]
+#_arpabet = ['@' + s for s in cmudict.valid_symbols]
 
 # Export all symbols:
 symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet
