0141b29afe7bfe4422f28cfdb6f32e0046c110d6
Antti Alastalo <antti.alastalo@aalto.fi>
2019-10-30 14:02:24 +0200

    Added finnish cleaners to hparams.py

--- hparams.py
+++ hparams.py
@@ -30,7 +30,8 @@ def create_hparams(hparams_string=None, verbose=False):
         validation_files='../data/isot_persot_val_filelist.txt',
         #training_files='filelists/ljs_audio_text_train_filelist.txt',
         #validation_files='filelists/ljs_audio_text_val_filelist.txt',
-        text_cleaners=['english_cleaners'],
+        #text_cleaners=['english_cleaners'],
+        text_cleaners=['finnish_cleaners'],
 
         ################################
         # Audio Parameters             #
