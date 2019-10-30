9bd7193f06ecb43d0878f2b64bada1e4f24fd479
Antti Alastalo <antti.alastalo@aalto.fi>
2019-10-30 14:08:57 +0200

    added GST hyperparameters and GST layers to tacotron2 model.

--- hparams.py
+++ hparams.py
@@ -79,6 +79,20 @@ def create_hparams(hparams_string=None, verbose=False):
         postnet_kernel_size=5,
         postnet_n_convolutions=5,
 
+        #GST parameters
+        # reference encoder
+        E = 512,
+        ref_enc_filters = [32, 32, 64, 64, 128, 128],
+        ref_enc_size = [3, 3],
+        ref_enc_strides = [2, 2],
+        ref_enc_pad = [1, 1],
+        ref_enc_gru_size = 512 // 2,
+
+        # Style Token Layer
+        token_num = 10,
+        num_heads = 8,
+        n_mels = 80,
+
         ################################
         # Optimization Hyperparameters #
         ################################
